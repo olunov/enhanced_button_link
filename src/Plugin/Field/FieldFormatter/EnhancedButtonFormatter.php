e6ead0a46b16a2ff0ebc7bad02e577d53c70cc77
Marton Balogh <marton_balogh@epam.com>
2020-02-05 10:26:28 +0100

    drupal 8 coding standards applied

--- src/Plugin/Field/FieldFormatter/EnhancedButtonFormatter.php
+++ src/Plugin/Field/FieldFormatter/EnhancedButtonFormatter.php
@@ -17,35 +17,31 @@ use Drupal\link\Plugin\Field\FieldFormatter\LinkFormatter;
  *   }
  * )
  */
-class EnhancedButtonFormatter extends LinkFormatter
-{
+class EnhancedButtonFormatter extends LinkFormatter {
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function defaultSettings()
-    {
-        return [
-        'btn_type' => 'btn-light',
-        'btn_size' => 'btn-sm',
-        'btn_status' => 'active',
-        'target' => '',
-        ] + parent::defaultSettings();
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'btn_type' => 'btn-light',
+      'btn_size' => 'btn-sm',
+      'btn_status' => 'active',
+      'target' => '',
+    ] + parent::defaultSettings();
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function settingsForm(array $form, FormStateInterface $form_state)
-    {
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $settings = $this->getSettings();
 
-        $settings = $this->getSettings();
-    
-        $form['btn_type'] = [
-        '#type' => 'select',
-        '#title' => $this->t('Button type'),
-        '#default_value' => $settings['btn_type'],
-        '#options' => [
+    $form['btn_type'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Button type'),
+      '#default_value' => $settings['btn_type'],
+      '#options' => [
         'btn-primary' => $this->t('Primary button'),
         'btn-secondary' => $this->t('Secondary button'),
         'btn-success' => $this->t('Success button'),
@@ -54,87 +50,84 @@ class EnhancedButtonFormatter extends LinkFormatter
         'btn-info' => $this->t('Info button'),
         'btn-light' => $this->t('Light button'),
         'btn-dark' => $this->t('Dark button'),
-        ],
-        '#required' => true,
-        ];
+      ],
+      '#required' => true,
+    ];
 
-        $form['btn_size'] = [
-        '#type' => 'select',
-        '#title' => $this->t('Button Size'),
-        '#default_value' => $settings['btn_size'],
-        '#options' => [
+    $form['btn_size'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Button Size'),
+      '#default_value' => $settings['btn_size'],
+      '#options' => [
         'btn-lg' => $this->t('Large Button'),
         'btn-sm' => $this->t('Small Button'),
-        ],
-        '#required' => true,
-        ];
-    
-        $form['btn_status'] = [
-        '#type' => 'select',
-        '#title' => $this->t('Button status'),
-        '#default_value' => $settings['btn_status'],
-        '#options' => [
+      ],
+      '#required' => true,
+    ];
+
+    $form['btn_status'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Button status'),
+      '#default_value' => $settings['btn_status'],
+      '#options' => [
         'active' => $this->t('Active Button'),
         'disabled' => $this->t('Disabled Button'),
-        ],
-        '#required' => true,
-        ];
-    
-        $form['target'] = [
-        '#type' => 'select',
-        '#title' => $this->t('Target window'),
-        '#target' => $settings['target'],
-        '#default_value' => $settings['target'],
-        '#options' => [
+      ],
+      '#required' => true,
+    ];
+
+    $form['target'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Target window'),
+      '#target' => $settings['target'],
+      '#default_value' => $settings['target'],
+      '#options' => [
         '' => $this->t('Current window'),
         '_blank' => $this->t('New window'),
-        ],
-        ];
+      ],
+    ];
     
-        return $form + parent::settingsForm($form, $form_state);
+    return $form + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $settings = $this->getSettings();
+    $summary = [];
+
+    $summary[] = $this->t('Button type: @text', ['@text' => $settings['btn_type']]);
+    $summary[] = $this->t('Button size: @text', ['@text' => $settings['btn_size']]);
+    $summary[] = $this->t('Button status: @text', ['@text' => $settings['btn_status']]);
+    
+    if (!empty($settings['target'])) {
+      $summary[] = $this->t('Open link in new window');
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function settingsSummary()
-    {
-    
-        $settings = $this->getSettings();
-        $summary = [];
-    
-        $summary[] = $this->t('Button type: @text', ['@text' => $settings['btn_type']]);
-        $summary[] = $this->t('Button size: @text', ['@text' => $settings['btn_size']]);
-        $summary[] = $this->t('Button status: @text', ['@text' => $settings['btn_status']]);
-        if (!empty($settings['target'])) {
-            $summary[] = $this->t('Open link in new window');
-        }
+    return $summary;
+  }
 
-        return $summary;
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    $settings = $this->getSettings();
+
+    foreach ($items as $delta => $item) {
+      $url = $this->buildUrl($item);
+      $urlTitle = $url->toString();
+      $elements[$delta] = [
+        '#theme' => 'link-formatter-enhanced-button-link',
+        '#url_title' => $urlTitle,
+        '#url' => $url,
+        '#type' => $settings['btn_type'],
+        '#size' => $settings['btn_size'],
+        '#status' => $settings['btn_status'],
+      ];
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function viewElements(FieldItemListInterface $items, $langcode)
-    {
-        $elements = [];
-        $settings = $this->getSettings();
-    
-        foreach ($items as $delta => $item) {
-            $url = $this->buildUrl($item);
-            $urlTitle = $url->toString();
-            $elements[$delta] = [
-            '#theme' => 'link-formatter-enhanced-button-link',
-            '#url_title' => $urlTitle,
-            '#url' => $url,
-            '#type' => $settings['btn_type'],
-            '#size' => $settings['btn_size'],
-            '#status' => $settings['btn_status'],
-            ];
-        }
-
-        return $elements;
-    }
-
+    return $elements;
+  }
 }
