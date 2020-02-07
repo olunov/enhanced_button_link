95cb2feee0d825a6b1ade04e0b990ad90cb9fc12
alunyov <alunyov@103985.no-reply.drupal.org>
2020-02-07 13:00:56 +0000

    Issue #3111891 by alunyov, rksyravi, metaaa, igergg: Remove \Drupal call EnhancedButtonFormatter (Drupal 9 compatibility)

--- src/Plugin/Field/FieldFormatter/EnhancedButtonFormatter.php
+++ src/Plugin/Field/FieldFormatter/EnhancedButtonFormatter.php
@@ -2,9 +2,13 @@
 
 namespace Drupal\enhanced_button_link\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Path\PathValidatorInterface;
+use Drupal\Core\Utility\Token;
 use Drupal\link\Plugin\Field\FieldFormatter\LinkFormatter;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'enhanced_button_formatter' formatter.
@@ -18,6 +22,57 @@ use Drupal\link\Plugin\Field\FieldFormatter\LinkFormatter;
  * )
  */
 class EnhancedButtonFormatter extends LinkFormatter {
+
+  /**
+   * The token replacement instance.
+   *
+   * @var \Drupal\Core\Utility\Token
+   */
+  protected $token;
+
+  /**
+   * Constructs a new LinkFormatter.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Third party settings.
+   * @param \Drupal\Core\Path\PathValidatorInterface $path_validator
+   *   The path validator service.
+   * @param \Drupal\Core\Utility\Token $token
+   *   The token replacement instance.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, PathValidatorInterface $path_validator, Token $token) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings, $path_validator);
+    $this->token = $token;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('path.validator'),
+      $container->get('token')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -47,7 +102,7 @@ class EnhancedButtonFormatter extends LinkFormatter {
       $url = $this->buildUrl($item);
 
       // Load Link Title.
-      $link_title = \Drupal::token()->replace($item->title, [$entity->getEntityTypeId() => $entity], ['clear' => TRUE]);
+      $link_title = $this->token->replace($item->title, [$entity->getEntityTypeId() => $entity], ['clear' => TRUE]);
 
       // Create default output of the link.
       $element[$delta] = [
